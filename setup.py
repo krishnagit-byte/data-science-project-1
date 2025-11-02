32b05852d3fdfab825cab056ec622fbe2b806848
Krishna <krishnadubai06@gmail.com>
2025-11-02 14:43:57 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List 
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    ## this function will return the list of requirements
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+
+
+
+
+setup (
+    name='my_project',
+    version='0.0.1',
+    author = "krishna",
+    author_email = "krishnadubai06@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
